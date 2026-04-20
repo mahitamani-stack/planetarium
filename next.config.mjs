8803ae812d91799b267f522384d35a26b61d9600
Mahita Manikandan <mahitamanikandan@Mahitas-MacBook-Air.local>
2026-04-20 16:18:30 +0530

    fix turbopack webpack conflict

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactCompiler: true,
+  turbopack: {},
   webpack: (config) => {
     config.resolve.fallback = { fs: false, path: false };
     return config;
