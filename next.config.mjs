fad324ea9f14da8c833253bc7eb885c834ea928c
Mahita Manikandan <mahitamanikandan@Mahitas-MacBook-Air.local>
2026-04-20 16:16:27 +0530

    webpack fix for mediapipe

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactCompiler: true,
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, path: false };
+    return config;
+  },
 };
 
 export default nextConfig;
